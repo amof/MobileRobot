702f6b3e81264b941632002018305370e59c9264
Julien Delvaux <delvaux.ju@gmail.com>
2015-06-24 13:28:07 +0200

    Ajout du fichier BluetoothTest & modification du main pour tester le motorshield

--- MobileRobot_Embedded/MobileRobot_Embedded/main.c
+++ MobileRobot_Embedded/MobileRobot_Embedded/main.c
@@ -8,23 +8,30 @@
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include <avr/delay.h>
-#include "Uart.h"
+#include "AdafruitMotorShield.h"
+
+DCMotor M1={8,10,9};
+DCMotor M2={13,11,12};
+DCMotor M3={2,4,3};
+DCMotor M4={7,5,6};
 
 int main(void)
 {
-	UART_init(UART_BAUD_SELECT(9600,1000000UL));
-	
+
 	sei();
 	
+	AFMS_init();
+	AFMS_DCMotor_setSpeed(M1,150);
+	AFMS_DCMotor_run(M1,FORWARD);
+	AFMS_DCMotor_setSpeed(M2,150);
+	AFMS_DCMotor_run(M2,FORWARD);
+	AFMS_DCMotor_setSpeed(M3,150);
+	AFMS_DCMotor_run(M3,FORWARD);
+	AFMS_DCMotor_setSpeed(M4,150);
+	AFMS_DCMotor_run(M4,FORWARD);
     while(1)
     {
-        if(UART_datasReceived==1){ //Datas have been received from UART
-	        UART_datasReceived=0;
-	        char* tampon=UART_read_buffer();
-	        UART_tx(tampon);
-			UART_tx_launch();
-        }
+
 
     }
 }
